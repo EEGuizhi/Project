604ad5526940b5179c4a6a9f9b4ba554ce89e53f
Guizhi <99305855+EEGuizhi@users.noreply.github.com>
2023-03-12 20:38:15 +0800

    first Upload

---
+++ independent_study/test.py
@@ -0,0 +1,13 @@
+# IKE
+import os
+import time
+import torch
+
+def main():
+    pass
+
+def test():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ independent_study/train.py
@@ -0,0 +1,13 @@
+# IKE
+import os
+import time
+import torch
+
+def main():
+    pass
+
+def train():
+    pass
+
+if __name__ == "__main__":
+    main()
