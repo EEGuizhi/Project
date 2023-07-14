12360d9b246b866846647a51a05cb41855775f19
Guizhi <99305855+EEGuizhi@users.noreply.github.com>
2023-07-14 15:22:46 +0800

    Update loss.py
    remove coords loss

--- code/tools/loss.py
+++ code/tools/loss.py
@@ -7,15 +7,15 @@ class CustomLoss(nn.Module):
         super(CustomLoss, self).__init__()
         self.use_coord_loss = use_coord_loss
         self.heatmap_maker = heatmap_maker
-        self.mae_criterion = nn.L1Loss()
         self.bce_loss = nn.BCELoss()
+        # self.mae_criterion = nn.L1Loss()
 
-    def forward(self, pred_heatmap, label, label_heatmap):
+    def forward(self, pred_heatmap, label_heatmap):
         loss, pred_heatmap = self.get_heatmap_loss(pred_heatmap=pred_heatmap, label_heatmap=label_heatmap)
-        pred_coord = self.heatmap_maker.get_heatmap2sargmax_coord(pred_heatmap=pred_heatmap)
-        if self.use_coord_loss:
-            coord_loss = self.mae_criterion(pred_coord, label)
-            loss += 0.01*coord_loss
+        # if self.use_coord_loss:
+        #     pred_coord = self.heatmap_maker.get_heatmap2sargmax_coord(pred_heatmap=pred_heatmap)
+        #     coord_loss = self.mae_criterion(pred_coord, label)
+        #     loss += 0.01*coord_loss
         return loss
 
     def get_heatmap_loss(self, pred_heatmap:torch.Tensor, label_heatmap:torch.Tensor):
